cae542c4399157abeb9ee272a8c44c8c5c1146a2
zhangyan <18758266469@163.com>
2020-07-13 03:18:02 +0800

    add detector

--- detector.py
+++ detector.py
@@ -4,8 +4,13 @@
 # !@fileName: retinaFace.py
 import torch
 import os
+
+from torchvision.ops import nms
+
 from data import cfg_mnet, cfg_re50
+from layers.functions.prior_box import PriorBox
 from models.retinaface import RetinaFace
+from utils.box_utils import decode, decode_landm
 
 
 def check_keys(model, pretrained_state_dict):
@@ -45,12 +50,92 @@ def load_model(model, pretrained_path, load_to_gpu):
 
 
 class Detector:
-    def __init__(self, weight="./retinaFace_checkpoints/mobilenet0.25_Final.pth", use_cuda=1):
+    def __init__(self, weight="./retinaFace_checkpoints/mobilenet0.25_Final.pth", use_cuda=1, image_size=None):
         network = os.path.split(weight)[-1].split("_")[0]
-        cfg = None
+        device = torch.device("cpu" if use_cuda else "cuda")
+        self.device = device
+        self.cfg = None
         if network == "mobilenet0.25":
-            cfg = cfg_mnet
+            self.cfg = cfg_mnet
         elif network == "Resnet50":
-            cfg = cfg_re50
-        net = RetinaFace(cfg=cfg, phase='test')
+            self.cfg = cfg_re50
+        net = RetinaFace(cfg=self.cfg, phase='test')
         net = load_model(net, weight, use_cuda)
+        net.eval()
+        net = net.to(device)
+        self.net = net
+
+        priorbox = PriorBox(self.cfg, image_size=image_size)
+        priors = priorbox.forward()
+        priors = priors.to(device)
+        self.prior_data = priors.data
+
+        scale_box = torch.Tensor([image_size[1], image_size[0], image_size[1], image_size[0]])
+        self.scale_box = scale_box.to(device)
+
+        scale_landms = torch.Tensor([image_size[1], image_size[0], image_size[1], image_size[0],
+                                     image_size[1], image_size[0], image_size[1], image_size[0],
+                                     image_size[1], image_size[0]])
+        self.scale_landms = scale_landms.to(device)
+        self.resize = 1
+
+        self.confidence_threshold = 0.4
+        self.top_k = 2000
+        self.keep_top_k = 150
+        self.nms_threshold = 0.4
+        del net, priorbox, priors
+
+    def nonMaximumSuppression(self, boxes, landms, scores):
+        inds = torch.where(scores > self.confidence_threshold)[0]
+        boxes = boxes[inds]
+        landms = landms[inds]
+        scores = scores[inds]
+
+        # keep top-K before NMS
+        order = scores.argsort()[::-1][:self.top_k]
+        boxes = boxes[order]
+        landms = landms[order]
+        scores = scores[order]
+
+        # do NMS
+
+        keep = nms(boxes, scores, self.nms_threshold)
+        boxes = boxes[keep, :]
+        landms = landms[keep]
+
+        # keep top-K faster NMS
+        boxes = boxes[:self.keep_top_k, :]
+        landms = landms[:self.keep_top_k, :]
+        boxes = tonumpy(boxes)
+        landms = tonumpy(landms)
+        return boxes, landms
+
+    def __call__(self, image):
+        img = image - (104, 117, 123)
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img).unsqueeze(0)
+        img = img.to(self.device)
+
+        loc, conf, landms = self.net(img)
+        boxes = decode(loc.data.squeeze(0), self.prior_data, self.cfg['variance'])
+        boxes = boxes * self.scale_box / self.resize
+        boxes = boxes.cpu()
+        scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+        landms = decode_landm(landms.data.squeeze(0), self.prior_data, self.cfg['variance'])
+        landms = landms * self.scale_landms / self.resize
+        landms = landms.cpu()
+
+        boxes, landms = self.nonMaximumSuppression(boxes, landms, scores)
+
+        faces = list()
+        for box, landm in zip(boxes, landms):
+            face = crop_image(image, boxes)
+            eye_left_x, eye_left_y = change_coord(landm[0], landm[1], box[0], box[1])
+            eye_right_x, eye_right_y = change_coord(landm[2], landm[3], box[0], box[1])
+            face = warp_affine(image=face, x1=eye_left_x, y1=eye_left_y, x2=eye_right_x, y2=eye_right_y)
+            faces.append(face)
+        return faces
+
+
+if __name__ == '__main__':
+    detector = Detector()
